a588a64bade6b4ef6c637ba4225a65f38e977fe9
victoriarosebender <victoria.rose.bender@gmail.com>
2024-07-24 23:47:51 +0000

    Question C

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,2 +1,49 @@
 #add import
 
+import question_c
+
+
+def menu():
+    print("Final Exam Menu \n 1-List Analysis \n 2-Exit")
+    ##this is the menu
+
+
+
+
+        
+def run_menu(choice):        
+    if (choice == '1'):
+        max_list = 5 
+        counter = 0
+        my_list = []
+
+        print("Enter a 5 integer list.")
+        while(counter < max_list):
+            num = input("Enter an integer: ")
+            num = int(num)
+            my_list.append(num)
+            counter += 1
+        question_c.data_analysis(my_list) 
+
+        
+        
+    
+
+    elif (choice == '2'):
+        print('Exiting')
+
+    else:
+        print('Invalid Input')
+
+def user_controlled_loop(): 
+    choice = '1'
+       
+    while( choice != '2'):
+        menu()
+        choice = input("Enter Menu Option: ")
+        run_menu(choice)
+
+
+
+
+user_controlled_loop()
--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,21 @@
 #write functions here, don't add input('') statements here!
+
+
+def data_analysis(my_list):
+
+    my_list.sort()
+
+    min = my_list[0]
+
+    max = my_list[4]
+
+    total = 0
+    for i in my_list:
+        total += i
+
+    average = total / len(my_list)
+
+
+    print(f"Min: {min}\nMax: {max}\nTotal: {total}\nAvg: {average}")
+
+    
